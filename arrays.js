47b3ce33bf3e7608276e7b4ebc6d84277425bb73
Brady <kite.alva@gmail.com>
2022-08-19 11:05:38 -0600

    second commit

--- arrays.js
+++ arrays.js
@@ -5,10 +5,17 @@ const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
 
+const colorCopy = faveColors.slice()
+console.log(colorCopy)
+
+console.log( `==================================`)
+
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
+colorCopy.push('brown')
+console.log(colorCopy)
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -18,7 +25,13 @@ const faveColors = ['red', 'green', 'black']
 
 const numbers = [1, 2, 3, 4, 5]
 
+
 //CODE HERE
+const middleNums = numbers.slice(1, 4)
+
+console.log( `==================================`)
+
+console.log(middleNums)
 
 
 //////////////////PROBLEM 4////////////////////
@@ -34,3 +47,16 @@ const numbers = [1, 2, 3, 4, 5]
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
 // CODE HERE
+console.log( `==================================`)
+
+const answers = []
+
+  for(let i = 0; i < bigOrSmallArray.length; i++){
+  if( bigOrSmallArray[i] > 100){
+  answers.push(`big ${bigOrSmallArray[i]}`)
+  }else if( bigOrSmallArray[i] <= 100){
+  answers.push(`small ${bigOrSmallArray[i]}`)
+  }
+}
+
+console.log(answers)
